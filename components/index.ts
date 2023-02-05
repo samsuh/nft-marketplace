6ce28b5b7f5d9b46c4967d3b3a260e076250a2d5
Samuel Suh <samsuh@gmail.com>
2023-02-05 13:34:27 +0900

    added nft data populated by local jsondata

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,6 @@
 //otherwise have to write: import Navbar from '../components/navbar'
 
 export { default as Navbar } from './navbar'
+export { default as BaseLayout } from './layout/BaseLayout'
+export { default as NftItem } from './nft/item'
+export { default as NftList } from './nft/list'
